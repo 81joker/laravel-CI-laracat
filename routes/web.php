27c59ee6fdfed17936d84d42a20b7ebe450d143e
joker81 <nehad.al.timimi@gmail.com>
2025-11-10 18:39:07 +0100

    override users in route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ()
+ {
+    $users = App\Models\User::all();
+    return view('welcome', ['users'
+     => $users]
+    );
 });

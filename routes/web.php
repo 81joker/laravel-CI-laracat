463638399d710b42333f28a0911c0a034dd5dada
joker81 <nehad.al.timimi@gmail.com>
2025-11-10 19:00:41 +0100

    fix: handle no changes to commit in Laravel Pint workflow

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function ()
- {
-    $users = App\Models\User::all();
-    return view('welcome', ['users'
-     => $users]
-    );
+Route::get('/', function () {
+    return view('welcome');
 });
